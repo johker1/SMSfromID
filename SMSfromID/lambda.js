553d1d247a603f965ae5a2c7038521b19841689f
johker1 <30807923+johker1@users.noreply.github.com>
2018-07-13 13:24:25 +0100

    initial commit

---
+++ SMSfromID/lambda.js
@@ -0,0 +1,47 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
+
+let childrenid = event['childrenid'];
+
+	ddb.get({
+		TableName: 'children',
+		Key: { 'id': childrenid }
+	}, function (err, data) {
+		if (err) {
+			console.log(err)
+		} else {
+			let receiver = data['Item']['phone'];
+			let sender = 'AWS Lost Children';
+			let message = 'Your children is now with me'
+		}
+	});
+
+	let isPromotional = true;
+
+	console.log("Sending message", message, "to receiver", receiver);
+
+	sns.publish({
+		Message: message,
+		MessageAttributes: {
+			'AWS.SNS.SMS.SMSType': {
+				DataType: 'String',
+				StringValue: 'Promotional'
+			},
+			'AWS.SNS.SMS.SenderID': {
+				DataType: 'String',
+				StringValue: sender
+			},
+		},
+		PhoneNumber: receiver
+	}).promise()
+		.then(data => {
+			console.log("Sent message to", receiver);
+			callback(null, data);
+		})
+		.catch(err => {
+			console.log("Sending failed", err);
+			callback(err);
+		});
+}
